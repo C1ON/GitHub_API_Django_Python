bfeeecc2e326b4293a59e3f662ca74b088ba42dd
Caner <caneron6@gmail.com>
2022-08-25 02:24:27 +0300

    variable names changed

--- githubfinder/views.py
+++ githubfinder/views.py
@@ -10,15 +10,15 @@ def index(request):
     if request.method == "POST":
         githubname = request.POST.get("githubname")
         response_user = requests.get(url+githubname)
-        user_info = response_user.json()
+        user = response_user.json()
 
         response_repos = requests.get(url+githubname+"/repos")
-        repos_info = response_repos.json()
+        repos = response_repos.json()
 
-        if "message" in user_info:
+        if "message" in user:
             error = "Kullanıcı adı bulunamadı."
             return render(request,"index.html",{"error":error})
         else:
-            return render(request,"index.html",{"info":user_info,"repos":repos_info})
+            return render(request,"index.html",{"info":user,"repos":repos})
     else:
         return render(request,"index.html")
